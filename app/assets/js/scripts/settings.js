a168526641858c5c179ba0c1c218cca266ac8a89
Ucasplayer <canallucastropod@gmail.com>
2026-04-30 17:00:03 -0300

    Atualização no launcher

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1472,7 +1472,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/Oreon/OreonLauncher/releases.atom',
+        url: 'https://github.com/Ucasplayer/OreonLauncher/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
